c060cb502d197e937cb7033c523c05b4cb766201
XintongTheCoder <xintongmi@gmail.com>
2023-06-10 17:47:52 -0700

    update schema

--- server/controllers/offer.controller.js
+++ server/controllers/offer.controller.js
@@ -3,3 +3,4 @@ const offerModel = require('../models/offer.model');
 module.exports.getBountyOffers = (req, res) => {};
 module.exports.createOffer = (req, res) => {};
 module.exports.completeOffer = (req, res) => {};
+module.exports.getUserOffers = (req, res) => {};
