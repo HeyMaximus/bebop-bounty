48f8dfff37d38c3ffda950bf1d204e8a16a748e0
Dylan Williams <dylan.mi.williams@gmail.com>
2023-06-12 20:47:00 -0700

    added frame

---
+++ client/src/components/user-profile/TransactionHistory.jsx
@@ -0,0 +1,14 @@
+import React, { useContext, useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Host = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TransactionHistory = () => {
+  
+}
+
+export default TransactionHistory;
