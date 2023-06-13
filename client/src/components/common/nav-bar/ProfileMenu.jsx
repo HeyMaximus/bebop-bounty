3f5c1485ad8bd5148edabca526364ccfba336177
Sara Li <ruimin.li77@gmail.com>
2023-06-13 16:28:48 -0700

    set up sign out function, and it will clear userData in useContext after sign out

--- client/src/components/common/nav-bar/ProfileMenu.jsx
+++ client/src/components/common/nav-bar/ProfileMenu.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { getAuth, signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 import { ProfileMenuContainer, ProfileMenuHeader, UpArrow, ProfileMenuProperties } from './navbar.styled';
+import firebaseApp from '../../../firebase';
+import { GlobalContext } from '../../GlobalContext.jsx';
 
 export default function ProfileMenu() {
   const exampleImgURL = 'https://i.pinimg.com/736x/5b/91/44/5b914448091084b6aa3dc005fad52eba.jpg';
+  const auth = getAuth(firebaseApp);
+  const navigate = useNavigate();
+  const { setUserData } = useContext(GlobalContext);
 
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    signOut(auth)
+      .then(() => {
+        console.log('Sign out successful.');
+        setUserData({}); // clear current user data after sign out
+        navigate('/login');
+      })
+      .catch((error) => {
+        console.error('Error occurrs during sign out:', error);
+      });
+  };
   return (
     <>
       <UpArrow />
@@ -20,7 +39,7 @@ export default function ProfileMenu() {
           <button>Your Bounties</button>
           <button>Settings</button>
           <button>Dark Mode</button>
-          <button>Sign Out</button>
+          <button onClick={handleSignOut}>Sign Out</button>
         </ProfileMenuProperties>
       </ProfileMenuContainer>
     </>
