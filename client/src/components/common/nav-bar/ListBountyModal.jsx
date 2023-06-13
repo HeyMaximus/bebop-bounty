884788b45746afb614cd028a4945b7ef43745333
Benjamin Pak <benjaminjpak95@gmail.com>
2023-06-13 14:53:39 -0700

    Added properties to listbountymodal

--- client/src/components/common/nav-bar/ListBountyModal.jsx
+++ client/src/components/common/nav-bar/ListBountyModal.jsx
@@ -11,11 +11,13 @@ import {
   StyledListBountyTitle,
 } from './navbar.styled';
 
-export default function ListBounty({ showListBountyModal }) {
+export default function ListBountyModal({ showListBountyModal }) {
   return (
     <StyledListBountyContainer>
       <StyledListBountyOverlay />
       <StyledListBountyBody>
+
+        {/* ====== INSERT UNIQUE CONTENT BELOW HERE ====== */}
         <StyledListBountyCloseBtn type="button" onClick={() => showListBountyModal()}>
           X
         </StyledListBountyCloseBtn>
@@ -25,10 +27,30 @@ export default function ListBounty({ showListBountyModal }) {
             Bounty Title:
             <StyledListBountyTitleInput />
           </StyledListBountyTitle>
+          <div>
+            <select>Catgory
+              <option>-- Select Category --</option>
+              <option>Gadget</option>
+              <option>Clothing</option>
+              <option>Furniture</option>
+              <option>Miscelleneous</option>
+            </select>
+            <select>Condition</select>
+            <div>Deadline <input type="date"></input></div>
+          </div>
+          <div>Preferred Payment</div>
+          <div>
+            Upload An Image <input type="file" accept="image/png, image/jpeg, image/jpg" />
+          </div>
+          <div>
+            Description <textarea type="text" />
+          </div>
         </StyledListBountyContentContainer>
         <StyledSubmitListBounty className="list-bounty-btn" type="button">
           List Button
         </StyledSubmitListBounty>
+        {/* ====== INSERT UNIQUE CONTENT ABOVE HERE ====== */}
+
       </StyledListBountyBody>
     </StyledListBountyContainer>
   );
