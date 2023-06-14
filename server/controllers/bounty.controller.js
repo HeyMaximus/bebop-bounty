1a00ff5a9268f4f6e1d402968b0a80cf68a08d5c
XintongTheCoder <xintongmi@gmail.com>
2023-06-14 13:36:21 -0700

    set up endpoint for post bounty

--- server/controllers/bounty.controller.js
+++ server/controllers/bounty.controller.js
@@ -1,4 +1,13 @@
 const bountyModel = require('../models/bounty.model');
 
 module.exports.getBounties = (req, res) => {};
-module.exports.createBounty = (req, res) => {};
+module.exports.createBounty = async (req, res) => {
+  const bounty = req.body;
+  try {
+    await bountyModel.createBounty(bounty);
+    res.status(201).send('Bounty created');
+  } catch (err) {
+    console.error(err.message);
+    res.sendStatus(400);
+  }
+};
--- server/models/bounty.model.js
+++ server/models/bounty.model.js
@@ -1,6 +1,16 @@
 const { pool } = require('../db');
 
-module.exports.getBounties = (page, count, sortBy, sortDir, category, city, state, searchQuery) => {
+module.exports.getBounties = (
+  page,
+  count,
+  sortBy,
+  sortDir,
+  category,
+  city,
+  state,
+  searchQuery,
+  userID
+) => {
   // const queryStr =
   //   'SELECT * FROM bounty WHERE ($5 IS NULL OR category=$5) AND ($6 IS NULL OR city=$6) AND ($7 IS NULL OR state=$7) AND ($8 IS NULL OR description LIKE '') ORDER BY $3 $4 LIMIT $2 OFFSET (($1 - 1) * $2)';
   // return pool
@@ -11,5 +21,57 @@ module.exports.getBounties = (page, count, sortBy, sortDir, category, city, stat
   //   });
 };
 
-module.exports.getUserBounties = () => {};
-module.exports.createBounty = () => {};
+module.exports.createBounty = (bounty) => {
+  const {
+    buyerID,
+    name,
+    category,
+    description,
+    condition,
+    deadline,
+    price,
+    city,
+    state,
+    payment,
+    image,
+  } = bounty;
+  const insertedCols = [
+    'buyer_id',
+    'name',
+    'category',
+    'description',
+    'condition',
+    'deadline',
+    'price',
+    'city',
+    'state',
+  ];
+  const insertedValues = [
+    buyerID,
+    name,
+    category,
+    description,
+    condition,
+    deadline,
+    Number(price),
+    city,
+    state,
+  ];
+  if (payment) {
+    insertedCols.push('preferred_payment');
+    insertedValues.push(payment);
+  }
+  if (image) {
+    insertedCols.push('image');
+    insertedValues.push(image);
+  }
+  const queryCols = insertedCols.join(',');
+  const queryValues = insertedValues.map((val, index) => `$${index + 1}`).join(',');
+  const queryStr = `INSERT INTO bounty (${queryCols}) VALUES (${queryValues})`;
+  return pool
+    .query(queryStr, insertedValues)
+    .then((queryRes) => queryRes.rows)
+    .catch((err) => {
+      console.error('Query failed: create bounty', err.message);
+    });
+};
