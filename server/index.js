5cb9c75d01b8fac2e1e0e9230035e049c31ab0f3
XintongTheCoder <xintongmi@gmail.com>
2023-06-15 15:32:33 -0700

    remove comment-out for serving static content

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static(path.resolve('client', 'dist')));
+app.use(express.static(path.resolve('client', 'dist')));
 
 app.use('/api', router);
-// app.get('*', (req, res) => res.sendFile(path.resolve('client', 'dist', 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.resolve('client', 'dist', 'index.html')));
 
 module.exports = app;
